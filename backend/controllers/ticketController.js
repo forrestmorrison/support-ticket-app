a9b0aaf20c55a75b69b5d5147082bd942c986cbd
forrestmorrison <forrestmorrison@gmail.com>
2023-03-03 16:55:24 -0600

    notes backend

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -1,8 +1,7 @@
 const asyncHandler = require("express-async-handler")
 
-const User = require("../models/userModel")
-
 const Ticket = require("../models/ticketModel")
+const User = require("../models/userModel")
 
 // @desc    get user tickets
 // @route   GET /api/tickets
